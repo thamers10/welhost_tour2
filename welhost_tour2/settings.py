a7e9fc32381a7420512dae34a71d91965f6f8b0f
thamers10 <dr.thamers10@gmail.com>
2025-06-19 21:26:37 +0300

    3

--- welhost_tour2/settings.py
+++ welhost_tour2/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "sslserver",  # ← تأكد أنه موجود هنا 
     # أطراف ثالثة
     "rest_framework",
     "rest_framework_simplejwt.token_blacklist",  # JWT
